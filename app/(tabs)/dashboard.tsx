c4f0d8238a1103218cf3db0db0d4c805955346fe
Aryan Sawhney <aryansaw03@gmail.com>
2023-11-23 01:20:53 -0500

    created dashboard component

---
+++ app/(tabs)/dashboard.tsx
@@ -0,0 +1,20 @@
+import { View, Text, useColorScheme, StyleSheet, TextInput } from "react-native";
+import React from "react";
+import getThemeColors from "../../constants/Colors";
+import LocationSelectorHeader from "../../components/LocationSelectorHeader";
+
+const Dashboard = () => {
+	const colors = getThemeColors(useColorScheme());
+	const [city, setCity] = React.useState("Ashburn");
+	const [state, setState] = React.useState("VA");
+	return (
+		<View
+			className="flex-1 items-center"
+			style={{ backgroundColor: colors.background }}>
+			<LocationSelectorHeader city={city} state={state} />
+			<TextInput />
+		</View>
+	);
+};
+
+export default Dashboard;
