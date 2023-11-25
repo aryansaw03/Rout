dfea8ef1d92b9443f72c183cf1f46f31bfb52a7b
Aryan Sawhney <aryansaw03@gmail.com>
2023-11-25 15:03:25 -0500

    completed header ui

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, useColorScheme } from "react-native";
+import React from "react";
+import getThemeColors from "../constants/Colors";
+
+const Header = ({ children }: { children: React.ReactNode }) => {
+	const colors = getThemeColors(useColorScheme());
+	return (
+		<View
+			className="w-full h-36 pb-3 items-center justify-end"
+			style={[
+				styles.bottomShadow,
+				{ backgroundColor: colors.backgroundTint },
+			]}>
+			{children}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	bottomShadow: {
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.4,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+});
+
+export default Header;
